4b783e44e32f2e0e7b47c44943992d6f20ffd831
Hristijan Nastoski <nastoskih@gmail.com>
2024-06-11 22:49:44 +0200

    adjust cookie settings

--- controllers/auth.js
+++ controllers/auth.js
@@ -39,9 +39,7 @@ export const userLogin = async (req, res, next) => {
 
         res
             .cookie("access_token", token, {
-                domain: 'warehouse-frontend-x3m-labs.vercel.app',
                 httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
             })
             .status(200)
             .json(others);
